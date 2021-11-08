a39afa58770ae947718a10ce04a8abf174467127
Luis Remis <luis@remis.io>
2021-11-08 10:57:59 -0800

    Fix license header on files

--- comm/ConnClient.cc
+++ comm/ConnClient.cc
@@ -4,6 +4,7 @@
  * The MIT License
  *
  * @copyright Copyright (c) 2017 Intel Corporation
+ * @copyright Copyright (c) 2021 ApertureData Inc
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"),
