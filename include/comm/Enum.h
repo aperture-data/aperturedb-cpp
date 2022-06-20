ff5e0aa252f4010f63433e23947f0e365c397c28
Josh Stoddard <josh@aperturedata.io>
2022-06-20 14:22:41 +0000

    constexpr enum flags

--- include/comm/Enum.h
+++ include/comm/Enum.h
@@ -6,11 +6,11 @@
 
 #define ENUM_FLAGS(T1, T2) \
 enum class T1 : T2; \
-inline T1  operator~  (T1  a      ) { return static_cast<T1>(~static_cast<T2>(a)); } \
-inline T1  operator|  (T1  a, T1 b) { return static_cast<T1>((static_cast<T2>(a) | static_cast<T2>(b))); } \
-inline T1  operator&  (T1  a, T1 b) { return static_cast<T1>((static_cast<T2>(a) & static_cast<T2>(b))); } \
-inline T1  operator^  (T1  a, T1 b) { return static_cast<T1>((static_cast<T2>(a) ^ static_cast<T2>(b))); } \
-inline T1& operator|= (T1& a, T1 b) { return reinterpret_cast<T1&>((reinterpret_cast<T2&>(a) |= static_cast<T2>(b))); } \
-inline T1& operator&= (T1& a, T1 b) { return reinterpret_cast<T1&>((reinterpret_cast<T2&>(a) &= static_cast<T2>(b))); } \
-inline T1& operator^= (T1& a, T1 b) { return reinterpret_cast<T1&>((reinterpret_cast<T2&>(a) ^= static_cast<T2>(b))); } \
+constexpr inline T1  operator~  (T1  a      ) { return static_cast<T1>(~static_cast<T2>(a)); } \
+constexpr inline T1  operator|  (T1  a, T1 b) { return static_cast<T1>((static_cast<T2>(a) | static_cast<T2>(b))); } \
+constexpr inline T1  operator&  (T1  a, T1 b) { return static_cast<T1>((static_cast<T2>(a) & static_cast<T2>(b))); } \
+constexpr inline T1  operator^  (T1  a, T1 b) { return static_cast<T1>((static_cast<T2>(a) ^ static_cast<T2>(b))); } \
+constexpr inline T1& operator|= (T1& a, T1 b) { return reinterpret_cast<T1&>((reinterpret_cast<T2&>(a) |= static_cast<T2>(b))); } \
+constexpr inline T1& operator&= (T1& a, T1 b) { return reinterpret_cast<T1&>((reinterpret_cast<T2&>(a) &= static_cast<T2>(b))); } \
+constexpr inline T1& operator^= (T1& a, T1 b) { return reinterpret_cast<T1&>((reinterpret_cast<T2&>(a) ^= static_cast<T2>(b))); } \
 enum class T1 : T2
