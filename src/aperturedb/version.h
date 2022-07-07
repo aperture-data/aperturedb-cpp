598463f0e7b66993ea85fe0bcff03034bd9f24d2
luisremis <luis@aperturedata.io>
2022-07-07 08:44:08 -0700

    Apply suggestions from code review

--- src/aperturedb/version.h
+++ src/aperturedb/version.h
@@ -6,15 +6,8 @@
 
 #pragma once
 
-// When creating a new release, the last commit must the be one
-// updating this file to the right version.
-// Both the string version (VDMS_VERSION) and the
-// int versions (VDMS_VERSION_M*) must be updated for a new release.
-
 // Versioning
 
-// This is the version as a string
-#define VDMS_VERSION  ""
 
 // This is the version for comparison
 #define VDMS_VERSION_MAJOR  0
